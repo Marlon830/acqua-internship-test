1036f70cdf91e97f5741c365682bc94109c42cc9
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2024-06-23 00:11:31 +0200

    added features to the smartbar, it can now add, delete, rename and update todos

--- src/services/aiSmartBarService.ts
+++ src/services/aiSmartBarService.ts
@@ -27,7 +27,7 @@ export async function getUpdatedTodosAiCompletion(
       {
         role: 'user',
         content:
-          '[System] You will be provided a list of string in JSON format. They are Todo list and done Todo list. These lists are complete and you can not assume that something is missing. The user wants you to move tasks from a list to the other. Your task is to update the two lists. The user is always talking about existing tasks. The user wants either to move a task from todo to done, or to move it from done to todo. You must detect which tasks to moves because the user won\'t say it to you. You must detect where they go between todo and done based on what the user said to you. Think step by step, stay simple. Output in JSON format by following this template: "{"todo": [{updatedTodoList}],"done": [{updatedDoneList}]}". You must not output something that is not the requested JSON. You are not able to talk, you are only able to provide the requested JSON. Do not explain what you did. You can only provide one updated JSON.\n\n' +
+          '[System] You will be provided a list of string in JSON format. They are Todo list and done Todo list. The user wants you to either add, rename, delete or move a task. Your purpose is to update the two lists. Think step by step, stay simple. Output in JSON format by following this template: "{"todo": [{updatedTodoList}],"done": [{updatedDoneList}]}". You must not output something that is not the requested JSON. You are not able to talk, you are only able to provide the requested JSON. Do not explain what you did. You can only provide one updated JSON.\n\n' +
           JSON.stringify(todosLists) +
           ' ' +
           value,
